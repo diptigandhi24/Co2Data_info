5b813c633d804bacc1943b72db3637c8c5e423f9
diptigandhi24 <gandhidipti.s@gmail.com>
2022-09-19 11:44:09 +0530

    adding netlify function and netlify setting and UI app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
+  let [month, updateMonth] = useState("1");
+  let [year, updateYear] = useState("1980");
+
+  // async function getco2Data
+  async function handleClick() {
+    await fetch("/.netlify/functions/getco2", {
+      method: "POST",
+      body: JSON.stringify({
+        year: "2021",
+        month: "2",
+      }),
+    }).then((res) => console.log("response received is", res));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="info">
+        <div className="info-form">
+          <div className="info-textbox">
+            <label>
+              Year: <input type="text" placeholder="Birth Year" />
+            </label>
+            <label>
+              Months: <input type="text" placeholder="Birth Month" />
+            </label>
+          </div>
+          <button type="submit" onClick={handleClick}>
+            Get Info
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
