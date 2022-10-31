6548d2417e49aca1fc40a7a323b1c199f9a4d79b
diptigandhi24 <gandhidipti.s@gmail.com>
2022-10-31 11:04:43 +0530

    organising co2 data manually and Ui changes and query results are succesful with Severless function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
-import axios from "axios";
 import { useState } from "react";
 
 function App() {
   let [month, updateMonth] = useState("1");
   let [year, updateYear] = useState("1980");
-
-  // async function getco2Data
   async function handleClick() {
     await fetch("/.netlify/functions/getco2", {
       method: "POST",
@@ -15,23 +11,60 @@ function App() {
         year: "2021",
         month: "2",
       }),
-    }).then((res) => console.log("response received is", res));
+    })
+      .then((res) => res.json())
+      .then((data) => console.log("data", data));
   }
   return (
     <div className="App">
-      <div className="info">
-        <div className="info-form">
-          <div className="info-textbox">
-            <label>
-              Year: <input type="text" placeholder="Birth Year" />
-            </label>
-            <label>
-              Months: <input type="text" placeholder="Birth Month" />
-            </label>
+      <div>
+        <p>
+          Each year, human activities release more carbon dioxide into the
+          atmosphere than natural processes can remove.
+        </p>
+        <p>
+          By adding more carbon dioxide to the atmosphere, people are
+          supercharging the natural greenhouse effect, causing global
+          temperature to rise.
+        </p>
+        <p>
+          Threshold for global change in temperature is set no more than 1.5
+          degrees C above pre-industrial averages, before climate changes cause
+          disastrous, unpredictable effects. We are already at 1.3 degrees C
+        </p>
+        <h4>
+          Please Enter your birth-year and select birth Month to get the co2
+          data
+        </h4>
+        <div className="info">
+          <div className="info-form">
+            <div className="info-textbox">
+              <div>
+                <label>Birth-Year </label>
+                <input
+                  type="text"
+                  placeholder={`1980-${new Date().getFullYear()}`}
+                />
+              </div>
+              <div className="month-btn">
+                <button>Jan</button>
+                <button>Feb</button>
+                <button>Mar</button>
+                <button>Apr</button>
+                <button>May</button>
+                <button>Jun</button>
+                <button>Jul</button>
+                <button>Aug</button>
+                <button>Sep</button>
+                <button>Oct</button>
+                <button>Nov</button>
+                <button>Dec</button>
+              </div>
+            </div>
+            <button type="submit" onClick={handleClick}>
+              Get Info
+            </button>
           </div>
-          <button type="submit" onClick={handleClick}>
-            Get Info
-          </button>
         </div>
       </div>
     </div>
