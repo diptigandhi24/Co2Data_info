446543d0c6a1e0cbdb7857f9a737e00efe9139a6
diptigandhi24 <gandhidipti.s@gmail.com>
2022-11-11 18:29:01 +0530

    adding twiiter UI tad rather than loading in useEffects

--- src/routes/twitterContainer.jsx
+++ src/routes/twitterContainer.jsx
@@ -2,31 +2,28 @@ import React, { useEffect } from "react";
 
 const TwitterContainer = ({ bornYear, currentYear }) => {
   useEffect(() => {
-    const anchor = document.createElement("a");
-    anchor.setAttribute("class", "twitter-share-button");
-    anchor.setAttribute("data-size", "large");
-    anchor.setAttribute("data-show-count", "false");
-    anchor.setAttribute(
-      "data-text",
-      `Average co2 data at your born year was ${bornYear}ppm, now it is ${currentYear}ppm`
-    );
-    anchor.setAttribute(
-      "href",
-      "https://twitter.com/share?ref_src=twsrc%5Etfw"
-    );
-    document.getElementsByClassName("twitter-embed")[0].appendChild(anchor);
-
     const script = document.createElement("script");
     script.async = true;
     script.setAttribute("src", "https://platform.twitter.com/widgets.js");
     script.setAttribute("charset", "utf-8");
     document.getElementsByClassName("twitter-embed")[0].appendChild(script);
-  });
+  }, []);
 
   return (
     <>
       <section className="twitterContainer">
-        <div className="twitter-embed"></div>
+        <div className="twitter-embed">
+          <a
+            href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+            class="twitter-share-button"
+            data-size="large"
+            data-text={`Average co2 data at your born year was ${bornYear}ppm, now it is ${currentYear}ppm`}
+            data-hashtags="ClimateChange ,ClimateChangeIsReal"
+            data-show-count="false"
+          >
+            Tweet
+          </a>
+        </div>
       </section>
     </>
   );
